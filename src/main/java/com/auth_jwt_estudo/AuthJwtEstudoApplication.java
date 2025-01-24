0c60e5bc53d8f2813236576c54e98c76a6fb5f85
matheuscs0 <dev.matheuscs@gmail.com>
2025-01-24 17:06:03 -0300

    create project

---
+++ src/main/java/com/auth_jwt_estudo/AuthJwtEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.auth_jwt_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth_jwt_estudo/AuthJwtEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth_jwt_estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
