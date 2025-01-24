1c7213f80400cddc4441a45e6621d9b4b2d87ee9
matheuscs0 <dev.matheuscs@gmail.com>
2025-01-24 18:22:23 -0300

    feat: user entity created

---
+++ src/main/java/com/auth_jwt_estudo/domain/user/User.java
@@ -0,0 +1,26 @@
+package com.auth_jwt_estudo.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/auth_jwt_estudo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.auth_jwt_estudo.repositories;
+
+import com.auth_jwt_estudo.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
